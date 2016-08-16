2655bc24349a1880cf50345a6509d3f8ca0ba2f4
JavanC <fu6gjp4@gmail.com>
2016-08-16 23:28:10 +0800

    initial pokedex

---
+++ Pokedex/Pokedex/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Pokedex
+//
+//  Created by Javan on 2016/8/16.
+//  Copyright © 2016年 Javan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
