8cdb6b11e731f522bafff336d9cb1d0f60cbfe39
JavanC <fu6gjp4@gmail.com>
2016-09-29 00:42:40 +0800

    fix teach view upload 1.3 version

--- Pokedex/Pokedex/TeachViewController.swift
+++ Pokedex/Pokedex/TeachViewController.swift
@@ -44,9 +44,9 @@ class TeachViewController: UIViewController {
             page2TitleLabel.text = "HOW TO GET IV'S?"
             page2Label.text = "Step 1. Set your trainer level.\n\nStep 2. Slide the Arc Into position, you can double check CP, HP is in the range and needed for power up is correct.\n\nStep 3. Slide your pokemon current CP and HP value.\n\nAdvanced: If use \"Pro\" can correction correct level and accurate stats."
             page3TitleLabel.text = "GYM ABILITY?"
-            page3Label.text = "Choose your \"Fast attack\" and \"Charge attack\", different combinations let you know that your ability to pekemon.\n\nAttack and Defensive damage are calculated from the Gym \"60 seconds\", touch the value you can see details. Default opponent is LV.20 Pikachu (IV Max)."
+            page3Label.text = "Choose your \"Fast attack\" and \"Charge attack\", different combinations let you know that your pekemon ability.\n\nChoose opponent, Will simulate the fight until your blood is 0. Attack and Defensive values are percent of opponent's HP, touch the value you can see battle details."
             page4TitleLabel.text = "THE FORMULA?"
-            page4Label.text = "Become attacks: Charge attack Seconds +0.5\nBecome defend: All attacks Seconds +2.0, HP x 2\n\nDamage = Floor(½ Power * Attacker's_Attack/Defender's_Defense * STAB * Effectiveness)+ 1\nHere we'll ignore Effectiveness."
+            page4Label.text = "<Become Attacker>\nCharge attack Seconds +0.5\n-2 HP = +1 energy\n\n<Become Defender>\nAll attacks Seconds + 2.0\n-2 HP = +1 energy, HP x 2\n\nDamage = Floor(½ Power * Attacker's_Attack/Defender's_Defense * STAB * Effectiveness)+ 1"
             teachDoneButton.setTitle("Okay, I get it!", forState: .Normal)
         case .Chinese, .Austrian:
             page1TitleLabel.text = "什麼是IV值？"
@@ -54,9 +54,9 @@ class TeachViewController: UIViewController {
             page2TitleLabel.text = "如何使用IV計算?"
             page2Label.text = "Step 1. 設定你的訓練師等級。\n\nStep 2. 滑動曲線至正確角度，你可以根據CP值及HP值是否在區間內，還有強化需求是否相同，來確認你的曲線角度是否在正確位置。\n\n Step 3. 輸入當前的CP值及HP值即可。\n\n進階計算: 點擊Pro可以結合隊長分析，修正正確等級與三項屬性。"
             page3TitleLabel.text = "道館攻守能力?"
-            page3Label.text = "選擇\"快速攻擊\"及\"蓄力攻擊\"，不同的技能組合會影響你的寶可夢在進攻道館跟防守道館的能力。\n\n選擇後會立刻根據能力值及技能計算在進攻道館及防守道館\"60秒總傷害值\"，點擊數值可以查看詳細資訊，預設對手皆為 Lv.20 皮卡丘(IV Max)。"
+            page3Label.text = "選擇\"快速攻擊\"及\"蓄力攻擊\"，不同的技能組合會影響你的寶可夢在進攻道館跟防守道館的能力。\n\n並可選擇對手模擬對戰，直到當你血量歸零，顯示造成對手多少血量百分比傷害。點擊數值可以查看詳細資訊。"
             page4TitleLabel.text = "計算公式?"
-            page4Label.text = "進攻方: 蓄力攻擊秒數+0.5\n防守方: 所有攻擊秒數+2.0，HP x 2\n\nDamage = Floor(½ Power * Attacker's_Attack/Defender's_Defense * STAB * Effectiveness)+ 1\n在此忽略Effectiveness影響。"
+            page4Label.text = "<進攻方>\n蓄力攻擊秒數+0.5\n-2 血量 = +1 能量\n<防守方>\n所有攻擊秒數+2.0\n-2 血量 = +1 能量、HP x 2\n\nDamage = Floor(½ Power * Attacker's_Attack/Defender's_Defense * STAB * Effectiveness)+ 1"
             teachDoneButton.setTitle("好的，我了解了！", forState: .Normal)
         }
     }
